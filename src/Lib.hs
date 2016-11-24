d1e5113d62db369a9460556d806cab0883ef665c
dchagniot <didier@chagniot.de>
2016-11-24 17:43:12 +0100

    beginning the toJson instance; prb with HECs

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,6 +10,33 @@ import Data.Aeson.TH
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
+import GHC.RTS.Events
+import GHC.Arr
+
+import Events.HECs
+import Events.ReadEvents
+
+--data HEC = (HECs, String, Int, Double)
+
+data MyHEC = MyHEC
+  { a        :: HECs
+  , b :: String
+  , c  :: Int
+  , d :: Double
+  }
+
+$(deriveJSON defaultOptions ''CapsetType)
+$(deriveJSON defaultOptions ''KernelThreadId)
+$(deriveJSON defaultOptions ''MessageTag)
+$(deriveJSON defaultOptions ''ThreadStopStatus)
+$(deriveJSON defaultOptions ''EventInfo)
+$(deriveJSON defaultOptions ''Event)
+$(deriveJSON defaultOptions ''CapEvent)
+$(deriveJSON defaultOptions ''HECs)
+$(deriveJSON defaultOptions ''(Array (Int CapEvent)))
+$(deriveJSON defaultOptions ''MyHEC)
+
+type API2 = "myHEC" :> Get '[JSON] [MyHEC]
 
 data User = User
   { userId        :: Int
